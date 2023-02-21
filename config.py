8e81ae79ec549f7bfe3b208c523554a5483799b4
Khaled Mohamed <46958133+xElkomy@users.noreply.github.com>
2023-02-22 01:19:17 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 # Slack webhooks for notifications
-posting_webhook = "https://discord.com/api/webhooks/997511728759844915/D_ThpS8KldsJQS0Wwep6RvS-rn_NRLFSuJbqWdZs4gli-sxcLilaAsWQ3tjiBmXHACaS"
-errorlogging_webhook = "https://discord.com/api/webhooks/997511728759844915/D_ThpS8KldsJQS0Wwep6RvS-rn_NRLFSuJbqWdZs4gli-sxcLilaAsWQ3tjiBmXHACaS"
+posting_webhook = "https://discord.com/api/webhooks/**********"
+errorlogging_webhook = "https://discord.com/api/webhooks//**********"
 # bypass Slack rate limit when using free workplace, switch to False if you're using Pro/Ent version.
 discord_sleep_enabled = True
 # Add @channel notifications to Slack messages, switch to False if you don't want to use @channel
